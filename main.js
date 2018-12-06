239a7388a80eb8359126c838454e838574130a9f
samurk <40658314+samurk@users.noreply.github.com>
2018-12-06 01:32:08 -0800

    Mock JS File & Index File

---
+++ main.js
@@ -0,0 +1,27 @@
+var accountinfof = {
+
+    title: "Sami-Ur-Rehman Khan",
+    balance: 1000,
+    currency: "PKR",
+    IBAN: "PK456987123",
+};
+
+function f() {
+    document.getElementById("title").innerText=accountinfof.title;
+    document.getElementById("balance").innerText=accountinfof.balance;
+    document.getElementById("currency").innerText=accountinfof.currency;
+    document.getElementById("IBAN").innerText=accountinfof.IBAN;
+}
+
+function deposit()
+{
+
+}
+
+function withdraw()
+{
+
+}
+
+f();
+
